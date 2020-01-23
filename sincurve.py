27b9bd872b57d7c3300826a76aa07608936aba66
Farhan Zaman <55164384+farhanzaman2000@users.noreply.github.com>
2020-01-23 16:56:20 -0500

    Update sincurve.py

--- sincurve.py
+++ sincurve.py
@@ -3,54 +3,31 @@ import numpy as np
 import matplotlib.pyplot as plot
 
 
-# Get x values of the sine wave
-
 time = np.arange(0, 10, 0.1);
 
- 
-
-# Amplitude of the sine wave is sine of a variable like time
+# Amplitude of the sine wave 
 
 amplitude   = np.sin(time)
 
- 
-
-# Plot a sine wave using time and amplitude obtained for the sine wave
-
 plot.plot(time, amplitude)
 
- 
-
-# Give a title for the sine wave plot
+# TITLE
 
 plot.title('Sine wave')
 
- 
-
-# Give x axis label for the sine wave plot
+# X-AXIS
 
 plot.xlabel('Time')
 
- 
-
-# Give y axis label for the sine wave plot
-
+# Y-AXIS
 plot.ylabel('Amplitude = sin(time)')
-
- 
 
 plot.grid(True, which='both')
 
- 
-
 plot.axhline(y=0, color='k')
-
- 
 
 plot.show()
 
- 
 
-# Display the sine wave
 
 plot.show()
